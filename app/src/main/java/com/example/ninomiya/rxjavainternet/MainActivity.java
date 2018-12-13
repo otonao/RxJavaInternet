f8e82754326c126eedb429f86231d206f8e1ed3e
DESKTOP-A33VV9I\Ninomiya <ootobi.naoto@gmail.com>
2018-12-13 15:58:59 +0900

    仮完成

--- app/src/main/java/com/example/ninomiya/rxjavainternet/MainActivity.java
+++ app/src/main/java/com/example/ninomiya/rxjavainternet/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.ninomiya.rxjavainternet;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
@@ -31,44 +36,42 @@ public class MainActivity extends AppCompatActivity {
         scrollView = findViewById(R.id.scroll);
 
         Button getButton = findViewById(R.id.htmlGetButton);
-        getButton.setOnClickListener(v -> asyncProcess());
+        getButton.setOnClickListener(v -> asyncProcess(this));
 
         Button clearButton = findViewById(R.id.clearButton);
         clearButton.setOnClickListener(v -> textView.setText(null));
     }
 
-    public void asyncProcess() {
+    public void asyncProcess(Context context) {
+        long start = System.currentTimeMillis();
 
         Observer<String> observer = new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
-                System.out.println("START");
             }
 
             @Override
             public void onNext(String s) {
-                System.out.println("SUCCESS" + s);
                 textView.append(s + "\n\n");
                 scrollView.scrollTo(0, textView.getBottom());
             }
 
             @Override
             public void onError(Throwable e) {
-                System.out.println("ERROR");
-                System.out.println(e.toString());
                 textView.append("ERROR\n");
                 textView.append(e.toString() + "\n");
             }
 
             @Override
             public void onComplete() {
-                System.out.println("FINISH");
-                textView.append("FINISH");
+                long end = System.currentTimeMillis();
+                Toast.makeText(context, (end - start)  + "ms", Toast.LENGTH_LONG).show();
                 scrollView.scrollTo(0, textView.getBottom());
             }
         };
 
         Observable.fromArray(urls)
+                .take(20)
                 .subscribeOn(Schedulers.io())
                 .map(HtmlGet::htmlGet)
                 .map(HtmlGet::titleGet)
