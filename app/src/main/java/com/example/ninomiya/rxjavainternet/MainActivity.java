2253a6fca1c96f615960fc1210d84a93516771e7
DESKTOP-A33VV9I\Ninomiya <ootobi.naoto@gmail.com>
2018-12-13 14:36:13 +0900

    研究中

--- app/src/main/java/com/example/ninomiya/rxjavainternet/MainActivity.java
+++ app/src/main/java/com/example/ninomiya/rxjavainternet/MainActivity.java
@@ -17,55 +17,63 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private ScrollView scrollView;
+    String[] urls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Resources res = getResources();
+        urls = res.getStringArray(R.array.kosen_url);
+
         textView = findViewById(R.id.myTextView);
         scrollView = findViewById(R.id.scroll);
 
-        Resources res = getResources();
-        String[] urls = res.getStringArray(R.array.kosen_url);
-
         Button getButton = findViewById(R.id.htmlGetButton);
-        getButton.setOnClickListener(v -> Observable.fromArray(urls)
+        getButton.setOnClickListener(v -> asyncProcess());
+
+        Button clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(v -> textView.setText(null));
+    }
+
+    public void asyncProcess() {
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("START");
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("SUCCESS" + s);
+                textView.append(s + "\n\n");
+                scrollView.scrollTo(0, textView.getBottom());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("ERROR");
+                System.out.println(e.toString());
+                textView.append("ERROR\n");
+                textView.append(e.toString() + "\n");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("FINISH");
+                textView.append("FINISH");
+                scrollView.scrollTo(0, textView.getBottom());
+            }
+        };
+
+        Observable.fromArray(urls)
                 .subscribeOn(Schedulers.io())
                 .map(HtmlGet::htmlGet)
                 .map(HtmlGet::titleGet)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        System.out.println("START");
-                    }
+                .subscribe(observer);
 
-                    @Override
-                    public void onNext(String s) {
-                        System.out.println("SUCCESS" + s);
-                        textView.append(s + "\n\n");
-                        scrollView.scrollTo(0, textView.getBottom());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        System.out.println("ERROR");
-                        System.out.println(e.toString());
-                        textView.append("ERROR\n");
-                        textView.append(e.toString() + "\n");
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        System.out.println("FINISH");
-                        textView.append("FINISH");
-                        scrollView.scrollTo(0, textView.getBottom());
-                    }
-
-                }));
-
-        Button clearButton = findViewById(R.id.clearButton);
-        clearButton.setOnClickListener(v -> textView.setText(""));
     }
 }
